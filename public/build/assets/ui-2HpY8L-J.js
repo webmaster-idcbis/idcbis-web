5d7c1b708afacbbc0cee4ae4190695925128bf84
SR1100 <webmaster@idcbis.org.co>
2026-08-07 18:46:26 -0500

    Subida de Build para cVue

---
+++ public/build/assets/ui-2HpY8L-J.js
@@ -0,0 +1,1 @@
+import{K as l,e as t}from"./app-DIbi-9R1.js";const u=l("ui",()=>{const s=t(null);return{flash:s,setFlash:(e,a="success")=>{s.value={message:e,type:a,id:Date.now()}},clearFlash:()=>{s.value=null}}});export{u};
